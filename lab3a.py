047dfc0798cd816b4b620a2e6a2a5978430f335e
dpVaghela <125076310+dpVaghela@users.noreply.github.com>
2023-02-11 19:54:55 -0500

    Create lab3a.py

---
+++ lab3a.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3 
+
+def write_text_file(file_name, c_name):
+	f = open(file_name, 'w')
+	f.write('Line 1\nLine 2 is a little longer\nLine 3 is as well\n')
+	f.write('This is the 4th line\n')
+	f.write('Last line in file\n')
+	f.write('copyright@' + c_name)
+	f.close()
+	print("File written successfully!")
+
+def read_file_string(file_name):
+        #Takes file_name as string for a file name, returns its entire contents as a string 
+        f = open(file_name, 'r')
+        return f.read()
+
+if __name__ == '__main__':
+	file_name = input("Enter the file name you want to write .txt:")
+	c_name = input("Enter your name: ")
+	write_text_file(file_name, c_name)
+	print(read_file_string(file_name))                                                                                                                                        1,17          Top
